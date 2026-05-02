8ff3ef170cf8dd7f5e48686b280d8e740024d50b
Oleksandr Solodukhin <78688752+Alex-Solod@users.noreply.github.com>
2026-05-02 20:51:28 +0300

    fix: remove validation annotations from response DTOs

--- src/main/java/mate/academy/spring_boot_security/dto/book/BookDtoWithoutCategoryIds.java
+++ src/main/java/mate/academy/spring_boot_security/dto/book/BookDtoWithoutCategoryIds.java
@@ -1,26 +1,15 @@
 package mate.academy.spring_boot_security.dto.book;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import java.math.BigDecimal;
 
 @Data
 public class BookDtoWithoutCategoryIds {
     private Long id;
-
-    @NotBlank
     private String title;
-
-    @NotBlank
     private String author;
-
-    @NotBlank
     private String isbn;
-
-    @NotNull
     private BigDecimal price;
-
     private String description;
     private String coverImage;
 }
