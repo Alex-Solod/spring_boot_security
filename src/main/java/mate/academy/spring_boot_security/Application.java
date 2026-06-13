db809326ae0466abd790f37a5c7547c744de591d
Oleksandr Solodukhin <78688752+Alex-Solod@users.noreply.github.com>
2026-06-13 03:14:04 +0300

    config: setup MySQL for main profile, move H2 to tests and use DEBUG_PORT variable

--- src/main/java/mate/academy/spring_boot_security/Application.java
+++ src/main/java/mate/academy/spring_boot_security/Application.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone("UTC"));
         SpringApplication.run(Application.class, args);
     }
-
 }
